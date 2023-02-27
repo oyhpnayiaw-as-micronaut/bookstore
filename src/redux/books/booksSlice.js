002e59664a0370c82530c3124e03f92a0d384d14
EBrain <ebrain.ebrain@gmail.com>
2023-02-27 23:41:10 +0630

    Change initial state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,17 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = {
+  list: [],
+};
 
 const slice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     bookAdded: (state, action) => {
-      state.push(action.payload);
+      state.list.push(action.payload);
     },
     bookRemoved: (state, action) => {
-      const index = state.findIndex((book) => book.id === action.payload.id);
-      state.splice(index, 1);
+      state.list = state.list.filter((e) => e.id !== action.payload.id);
     },
   },
 });
