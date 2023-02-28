8aec411ca6ef02f6ba08082132f6e535cddd30a2
EBrain <ebrain.ebrain@gmail.com>
2023-02-28 18:34:32 +0630

    Dispatch action for category page

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectCategorystatus,
+  statusChecked,
+} from 'src/redux/categories/categoriesSlice';
 
 import Button from '../shared/Button';
 
 function Categories() {
+  const dispatch = useDispatch();
+  const status = useSelector(selectCategorystatus);
+
+  const handleClick = () => {
+    dispatch(statusChecked());
+  };
+
   return (
     <div>
-      <Button>Check status</Button>
+      {status && <p>{status}</p>}
+      <Button onClick={handleClick}>Check status</Button>
     </div>
   );
 }
