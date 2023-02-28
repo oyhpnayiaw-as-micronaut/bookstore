a05aa84997eac701849b041d06686433123ae3d2
EBrain <ebrain.ebrain@gmail.com>
2023-02-28 18:29:17 +0630

    Change to controlled component

--- src/components/books/BookNewForm.js
+++ src/components/books/BookNewForm.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { bookAdded } from 'src/redux/books/booksSlice';
 import Button from '../shared/Button';
+import Input from '../shared/Input';
+
+const initialState = { title: '', author: '' };
 
 function BookNewForm() {
   const dispatch = useDispatch();
+  const [state, setState] = useState(initialState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-
-    dispatch(bookAdded(data));
-    e.target.reset();
+    dispatch(bookAdded(state));
+    setState(initialState);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name="title" required placeholder="Book Title" />
-      <input name="author" required placeholder="Author" />
+      <Input
+        name="title"
+        placeholder="Book Title"
+        value={state.title}
+        setValue={setState}
+        required
+      />
+      <Input
+        name="author"
+        placeholder="Author"
+        value={state.author}
+        setValue={setState}
+        required
+      />
       <Button type="submit">Add Book</Button>
     </form>
   );
