50e05a8b4763431c3bdbb368767e57433f17edef
EBrain <ebrain.ebrain@gmail.com>
2023-02-28 20:40:42 +0630

    Create api

---
+++ src/api.js
@@ -0,0 +1,47 @@
+import { API_URL } from './config';
+
+export const getBooks = async () => {
+  const res = await fetch(API_URL);
+
+  if (!res.ok) return [];
+
+  const contentLength = res.headers.get('content-length');
+
+  if (res.ok && contentLength === '0') return [];
+
+  const json = await res.json();
+
+  const data = Object.entries(json).reduce((acc, [id, v]) => {
+    acc.push({ ...v[0], id });
+    return acc;
+  }, []);
+
+  data.sort((a, b) => a.id.localeCompare(b.id));
+
+  return data;
+};
+
+export const postBook = async (book) => {
+  const id = `${Date.now()}-${crypto.randomUUID()}`;
+
+  const data = {
+    ...book,
+    category: 'Category',
+  };
+
+  await fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify({ ...data, item_id: id }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+
+  return { ...data, id };
+};
+
+export const deleteBook = async (id) => {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE',
+  });
+};
---
+++ src/config.js
@@ -0,0 +1,3 @@
+// export const APP_ID = '8mijvvtXyIaeIkWIjx1O';
+export const APP_ID = '7jFvZsJvO7rQyvqV2Pqh';
+export const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}/books`;
