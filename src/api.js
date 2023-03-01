9ce2098ee17bdc407673c7179d3b82a543c43f5c
EBrain <ebrain.ebrain@gmail.com>
2023-03-01 23:40:15 +0630

    Refactor api

--- src/api.js
+++ src/api.js
@@ -6,16 +6,10 @@ export const getBooks = async () => {
   if (!res.ok) return [];
 
   const contentLength = res.headers.get('content-length');
-
   if (res.ok && contentLength === '0') return [];
 
   const json = await res.json();
-
-  const data = Object.entries(json).reduce((acc, [id, v]) => {
-    acc.push({ ...v[0], id });
-    return acc;
-  }, []);
-
+  const data = Object.entries(json).map(([id, v]) => ({ ...v[0], id }));
   data.sort((a, b) => a.id.localeCompare(b.id));
 
   return data;
@@ -24,17 +18,12 @@ export const getBooks = async () => {
 export const postBook = async (book) => {
   const id = `${Date.now()}-${crypto.randomUUID()}`;
 
-  const data = {
-    ...book,
-    category: 'Category',
-  };
+  const data = { ...book, category: 'Category' };
 
   await fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify({ ...data, item_id: id }),
-    headers: {
-      'Content-type': 'application/json',
-    },
+    headers: { 'Content-type': 'application/json' },
   });
 
   return { ...data, id };
